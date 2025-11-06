e12ebdd2174017827ad9564fe628e09a8f4b8f15
Azizbek2333 <250424@newuu.uz>
2025-11-06 10:08:03 +0500

    Create problem2.cpp

---
+++ problem2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Number of numbers"<<endl;
+    cin >> n;
+    int a[n],sum=0;
+    cout<<"Enter numbers"<<" ";
+    for(int i=0;i<n;i++) {
+        cin >> a[i];
+        sum+=a[i];
+
+    }
+
+    cout<<"average"<<sum/double(n)<<endl;
+
+
+
+
+    return 0;
+}
